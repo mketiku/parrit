53254e2436b542bd8842007901b60a52eca6f426
michael ketiku <mketiku@gmail.com>
2026-04-06 08:50:20 -0400

    chore: shorted parrit card

--- api/og.tsx
+++ api/og.tsx
@@ -157,7 +157,7 @@ export default function handler(request: Request) {
                   display: 'flex',
                 }}
               >
-                beautifully organized.
+                simplified.
               </div>
               <div
                 style={{
